113f8851678a3ab4d829bbeb71db8d7de45abdd8
Renaud Pacalet <renaud.pacalet@telecom-paristech.fr>
2025-03-25 13:12:06 +0100

    update vhdl/kat_pkg.vhd build

--- local.mk
+++ local.mk
@@ -3,24 +3,11 @@ SIM  := ghdl
 GUI  := no
 V    := 1
 
-C := C
-BUILD := $(TOP)/build
-SCRIPTS := $(TOP)/scripts
-KAT_TO_VHDL := $(SCRIPTS)/kat_to_vhdl.awk
-KAT_FILE := $(BUILD)/LWC_AEAD_KAT_128_128.txt
 KAT_PKG := $(VHDL)/kat_pkg.vhd
-
-$(KAT_PKG): $(KAT_TO_VHDL) $(KAT_FILE)
-	awk -f $< $(KAT_FILE) > $@
-
-$(KAT_FILE): | $(BUILD)
-	git submodule init
-	git submodule update
-	cmake -S $(C) -B $(BUILD) -DALG_LIST="asconaead128" -DIMPL_LIST="ref" -DTEST_LIST="genkat"
-	cmake --build $(BUILD)
-	ctest --test-dir $(BUILD)
 
 ascon_pkg_sim.sim: $(KAT_PKG)
 
-$(BUILD):
-	mkdir -p build
+.PHONY: $(KAT_PKG)
+
+$(KAT_PKG):
+	$(MAKE) -C $(TOP) -f Makefile.kat
