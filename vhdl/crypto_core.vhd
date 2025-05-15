3d6fdcf3c767a9e75e2eeb86dcf45dad49db487a
Renaud Pacalet <renaud.pacalet@telecom-paris.fr>
2025-05-15 11:36:45 +0200

    add tag_valid output to crypto_core

--- vhdl/crypto_core.vhd
+++ vhdl/crypto_core.vhd
@@ -28,6 +28,7 @@ entity crypto_core is
     tlast_out:  out std_ulogic;
     key:        in  std_ulogic_vector(127 downto 0);
     nonce:      in  std_ulogic_vector(127 downto 0);
+    tag_valid:  out std_ulogic;
     tag:        out std_ulogic_vector(127 downto 0)
   );
 end entity crypto_core;
